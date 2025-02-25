ed25077350668072da92fcabc078d592db201d97
meronoumer <meron.oumer2005@gmail.com>
2025-02-24 21:27:51 -0500

    almost done testing code - done with javadoc headers and etc

--- Car.java
+++ Car.java
@@ -1,3 +1,93 @@
-public class Car {
+import java.util.ArrayList;
 
+public class Car implements CarRequirements {
+    private ArrayList passengers;
+    private int capacity;
+
+
+/**
+ * Constructs a Car object with the given list of passengers and capacity.
+ *
+     * @param passengers The initial list of passengers in the car.
+     * @param capacity   The maximum number of passengers the car can hold.
+
+ */
+public Car( ArrayList passengers, int Capacity) {
+    this.passengers = passengers; 
+    this.capacity = capacity; 
+}
+
+/**
+ * Returns the maximum passenger capacity of the car.
+ *
+ * @return The capacity of the car.
+ */
+public int getCapacity(){
+    return capacity;
+
+}
+
+/**
+ * Returns the number of seats remaining in the car.
+ *
+ * @return The number of available seats.
+ */
+public int seatsRemaining(){
+     
+    return this.capacity - this.passengers.size() ;
+
+}
+
+/**
+ * Adds a passenger to the car if there is available space.
+ *
+ * @param p The passenger to add.
+ * @return True if the passenger was added successfully, false otherwise.
+ */
+public Boolean addPassenger(Passenger p){
+    if (seatsRemaining() > 0 && !this.passengers.contains(p)){
+        this.passengers.add(p);
+        return true;}
+
+    else{
+        return false;
+    }
+    
+
+
+}
+
+/**
+ * Removes a passenger from the car if they are present.
+ *
+ * @param p The passenger to remove.
+ * @return True if the passenger was removed successfully, false otherwise.
+ */
+public Boolean removePassenger(Passenger p){
+    if(seatsRemaining() > 0 && this.passengers.contains(p)){
+        this.passengers.remove(p);
+        return true;
+        
+    }
+    else{
+        return false;
+    }
+
+}
+
+/**
+ * Prints the passenger manifest of the car.
+ */
+public void printManifest(){
+    if (this.passengers.size()>0){
+        System.out.println("----------------------");
+        System.out.println("Passenger Manifest");
+        for(Object person : passengers){
+            System.out.println(person);
+        }System.out.println("----------------------");
+    }
+    else{
+        System.out.println("This car is empty.") ;
+    }
+}
 }
--- Passenger.java
+++ Passenger.java
@@ -1,8 +1,51 @@
-public class Passenger {
+
+/**
+ * Passenger Class with a name and the ability to board and get off a Car.
+ */
+
+public class Passenger implements PassengerRequirements {
     
     private String name;
 
+    /**
+     * Constructs a Passenger object with the given name.
+     *
+     * @param name The name of the passenger.
+     */
     public Passenger(String name) {
         this.name = name;
     }
+
+    /**
+     * Attempts to board the given Car.
+     *
+     * @param c The Car to board.
+     */
+    public void boardCar(Car c){
+        if (c.addPassenger(this)){
+            System.out.println(this);
+        }
+        else
+        {
+            System.out.println("This car is full.");
+        }
+        
+    }
+
+    /**
+     * Attempts to get off the given Car.
+     *
+     * @param c The Car to get off.
+     */
+    public void getOffCar(Car c){
+        if(!c.removePassenger(this)){
+            System.out.println("Customer already off car");
+        }else{
+            System.out.println(this);
+        }
+    }
+
+
+
+
 }
